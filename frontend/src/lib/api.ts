1245fb66a2eaa78ab7aabc2e7590ae9848b7a083
Archie To <toanhminh0412@gmail.com>
2025-06-01 19:40:14 -0700

    Set API URL to be an env var

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 // Create axios instance
 const api = axios.create({
